52305adeb945a36a5c89ea760b6565bbb6729915
satyam kaurav <satyam_013@outlook.com>
2024-09-03 18:06:38 +0530

    d-01

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,13 @@
+const User = require("../models/User");
 const { serverError, success } = require("../utils/response");
 
 const userSignUp = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    return success(res, "Sign-up data", { username, email, password });
+    const user = await User.create({ username, email, password });
+
+    return success(res, "Sign-up data", user);
   } catch (error) {
     return serverError(res, error);
   }
