581ad2090898e10562ac05d5e0c1dd3c1387bc71
satyam kaurav <satyam_013@outlook.com>
2024-09-04 21:10:35 +0530

    uid

--- utils/u_id.js
+++ utils/u_id.js
@@ -1,46 +1,27 @@
-function nextChar(c, lowestbound, highestbond) {
-  return c === highestbond
-    ? lowestbound
-    : String.fromCharCode(c.charCodeAt(0) + 1);
-}
+// generate 2,176,782,336 starts from 000000
+function incr_str(str) {
+  const arr = str.split("");
 
-function incrementString(str) {
-  let arr = str.split("");
-  const length = arr.length;
-
-  for (let i = length - 1; i >= 0; i--) {
-    const current_char = arr[i];
-    if (current_char >= "0" && current_char <= "9") {
-      if (current_char < "9") {
-        arr[i] = nextChar(current_char, "0", "9");
-        break;
-      } else {
-        arr[i] = "0";
-        continue;
-      }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const item = arr[i];
+    if ((item >= "0" && item < "9") || (item >= "A" && item < "Z")) {
+      arr[i] = String.fromCharCode(item.charCodeAt(0) + 1);
+      break;
     }
-    if (current_char >= "a" && current_char <= "z") {
-      if (current_char < "z") {
-        arr[i] = nextChar(current_char, "a", "z");
-        break;
-      } else {
-        arr[i] = "a";
-        continue;
-      }
+    if (item === "9") {
+      arr[i] = "A";
+      break;
     }
-    if (current_char >= "A" && current_char <= "Z") {
-      if (current_char < "Z") {
-        arr[i] = nextChar(current_char, "A", "Z");
-        break;
-      } else {
-        arr[i] = "A";
-        continue;
-      }
+    if (item === "Z") {
+      arr[i] = "0";
+      continue;
     }
   }
+
   return arr.join("");
 }
 
-const new_str = incrementString("999zzzZZZ");
+const old_str = "000000";
+const new_str = incr_str(old_str);
 
-console.log(new_str);
+console.log({ old_str, new_str });
